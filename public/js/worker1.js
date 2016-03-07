1421180bd6ec28b78131ff1c53c3440ec34f43b8
MIURA <takashi.miura@soliton.co.jp>
2016-03-07 14:21:33 +0900

    力尽きた

--- public/js/worker1.js
+++ public/js/worker1.js
@@ -1,8 +1,14 @@
 onmessage = function(e) {
+	var baseTime = new Date().getTime();
 	var count = 0;
 	while(true) {
-		postMessage(count++);
-		if(count > 1000000) {
+		var currentTime = new Date().getTime();
+		var tmpTime = new Date().getTime();
+		while((tmpTime - currentTime) < 1000) {
+		}
+		var dif = currentTime - baseTime;
+		postMessage(dif);
+		if(dif > 1000000) {
 			break;
 		}
 	}
